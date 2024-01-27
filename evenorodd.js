68d2eefc491135044d1d3506290fb91b95b4cda6
Arkun Rifki <arkunrifki17@gmail.com>
2024-01-27 22:03:56 +0700

    update description 1/366

---
+++ evenorodd.js
@@ -0,0 +1,12 @@
+// Coding Challenge 2024 1/366
+// https://www.codewars.com/kata/53da3dbb4a5168369a0000fe
+
+function evenOrOdd(number) {
+  if (number % 2 === 0) {
+    return "Even";
+  } else {
+    return "Odd";
+  }
+}
+
+console.log(evenOrOdd(3));
