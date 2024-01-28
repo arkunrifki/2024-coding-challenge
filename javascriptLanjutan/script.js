d8dbf1bd1b7df620b10bc1a0efef9b4715eb84ac
Arkun Rifki <arkunrifki17@gmail.com>
2024-01-28 15:21:22 +0700

    day #5

---
+++ javascriptLanjutan/script.js
@@ -0,0 +1,59 @@
+// Cara untuk membuat Object pada Javascript
+// 1. Object Literal
+// let mahasiswa = {
+//   //Properti
+//   nama: "Fikri",
+//   energi: 10,
+
+//   //Method
+//   makan: function (porsi) {
+//     this.energi += porsi;
+//     console.log(`Halo ${this.nama}, selamat makan!`);
+//   },
+// };
+
+// let mahasiswa1 = {
+//   //Properti
+//   nama: "Samsul",
+//   energi: 20,
+
+//   //Method
+//   makan: function (porsi) {
+//     this.energi += porsi;
+//     console.log(`Halo ${this.nama}, selamat makan!`);
+//   },
+// };
+
+// console.log(mahasiswa);
+// console.log(mahasiswa.energi);
+// mahasiswa.makan(4);
+// console.log(mahasiswa.energi);
+
+// console.log(mahasiswa1);
+// console.log(mahasiswa1.energi);
+// mahasiswa1.makan(4);
+// console.log(mahasiswa1.energi);
+
+// 2. Function Declarration
+function Mahasiswa(nama, energi) {
+  let mahasiswa = {};
+  mahasiswa.nama = nama;
+  mahasiswa.energi = energi;
+
+  mahasiswa.makan = function (porsi) {
+    this.energi += porsi;
+    console.log(`Halo ${this.nama}, selamat makan!`);
+  };
+
+  mahasiswa.main = function (jam) {
+    this.energi -= jam;
+    console.log(`Halo ${this.nama}, selamat bermain!`);
+  };
+  return mahasiswa;
+}
+
+let arkun = Mahasiswa("Arkun", 10);
+let samsul = Mahasiswa("Samsul", 20);
+
+// 3. Constructor Declaration
+// 4. Object.create
