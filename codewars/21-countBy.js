4a09e2653fae51d25c4ca43984f6e931a6a25ab1
Arkun Rifki <arkunrifki17@gmail.com>
2024-04-26 10:13:09 +0700

    day-13

--- codewars/21-countBy.js
+++ codewars/21-countBy.js
@@ -1,11 +1,17 @@
-function countBy(x, n) {
-  let z = [];
+// function countBy(x, n) {
+//   let z = [];
 
-  for (let i = 1; i <= n; i++) {
-    z.push(x * i);
-  }
+//   for (let i = 1; i <= n; i++) {
+//     z.push(x * i);
+//   }
 
-  return z;
-}
+//   return z;
+// }
+
+// function countBy(x, n) {
+//   return [...Array(n)].map((_, i) => x * (i + 1));
+// }
+
+const countBy = (x, n) => [...Array(n)].map((_, i) => x * (i + 1));
 
 console.log(countBy(2, 5));
