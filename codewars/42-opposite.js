9e997c9c7079bd5eb83dfab0b43574674433edaf
Arkun Rifki <arkunrifki17@gmail.com>
2024-04-30 16:57:36 +0700

    day-32

---
+++ codewars/42-opposite.js
@@ -0,0 +1,15 @@
+/* 
+==============
+Learn Data Structure with JavaScript
+Coding Challenge 2024 32/366
+Challenge 2024 32. opposite
+==============
+*/
+
+/* 
+const opposite = n => n * -1
+*/
+
+const opposite = (number) => -number;
+console.log(opposite(0));
+console.log(opposite(4.25));
