904286d8a302e27fab8eaee6315cce574fc86a60
Arkun Rifki <arkunrifki17@gmail.com>
2024-04-30 23:36:20 +0700

    day-33

---
+++ codewars/43-summation.js
@@ -0,0 +1,19 @@
+/* 
+==============
+Learn Data Structure with JavaScript
+Coding Challenge 2024 33/366
+Challenge 2024 33. summation
+==============
+*/
+
+var summation = function (num) {
+  // Code here
+  let total = 0;
+  for (let i = 1; i <= num; i++) {
+    total += i;
+  }
+  return total;
+};
+
+console.log(summation(1));
+console.log(summation(2));
