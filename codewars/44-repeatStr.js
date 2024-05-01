0917f932b557e60012009530af1d4845a3271ccb
Arkun Rifki <arkunrifki17@gmail.com>
2024-05-01 21:32:49 +0700

    day-34

---
+++ codewars/44-repeatStr.js
@@ -0,0 +1,16 @@
+/* 
+==============
+Learn Data Structure with JavaScript
+Coding Challenge 2024 34/366
+Challenge 2024 34. repeatStr
+==============
+*/
+
+// function repeatStr(n, s) {
+//   return n > 0 ? s.repeat(n) : "";
+// }
+
+const repeatStr = (n, s) => s.repeat(n);
+
+console.log(repeatStr(3, "*"));
+console.log(repeatStr(3, "I"));
