9573439fb0e6fe84bd1f7138a5a736d951750b7f
Arkun Rifki <arkunrifki17@gmail.com>
2024-05-04 13:31:37 +0700

    day-38

---
+++ codewars/50-litres.js
@@ -0,0 +1,21 @@
+/* 
+==============
+Learn Data Structure with JavaScript
+Coding Challenge 2024 40/366
+Challenge 2024 40. litres
+==============
+*/
+
+// function litres(time) {
+//   var drinks = time * 0.5;
+//   if (time % 0.5 === 0) {
+//     return Math.floor(drinks);
+//   }
+//   return Math.floor(drinks);
+// }
+
+const litres = (time) => Math.floor(time * 0.5);
+console.log(litres(2));
+console.log(litres(3));
+console.log(litres(1.4));
+console.log(litres(12.3));
