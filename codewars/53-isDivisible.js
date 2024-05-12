e63e38d3ab3a8878081836a8ac72ad1998628f86
Arkun Rifki <arkunrifki17@gmail.com>
2024-05-12 21:46:32 +0700

    day-42

--- codewars/53-isDivisible.js
+++ codewars/53-isDivisible.js
@@ -9,3 +9,55 @@ Challenge 2024 42. isDivisible
 const isDivisible = (n, x, y) => n % x === 0 && n % y === 0;
 
 console.log(isDivisible(3, 3, 4));
+
+/* 
+==================================
+*/
+
+// Setup
+const contacts = [
+  {
+    firstName: "Akira",
+    lastName: "Laine",
+    number: "0543236543",
+    likes: ["Pizza", "Coding", "Brownie Points"],
+  },
+  {
+    firstName: "Harry",
+    lastName: "Potter",
+    number: "0994372684",
+    likes: ["Hogwarts", "Magic", "Hagrid"],
+  },
+  {
+    firstName: "Sherlock",
+    lastName: "Holmes",
+    number: "0487345643",
+    likes: ["Intriguing Cases", "Violin"],
+  },
+  {
+    firstName: "Kristian",
+    lastName: "Vos",
+    number: "unknown",
+    likes: ["JavaScript", "Gaming", "Foxes"],
+  },
+];
+
+function lookUpProfile(name, prop) {
+  // Only change code below this line
+  for (var i = 0; i < contacts.length; i++) {
+    if (contacts[i].firstName === name) {
+      return contacts[i][prop] || "No such property";
+    }
+  }
+  return "No such contact";
+
+  for (var i = 0; i < contacts.length; i++) {
+    if (contacts[i].firstName === name) {
+      return contacts[i][prop] || "No such property";
+    }
+  }
+  return "No such contact";
+  // Only change code above this line
+}
+
+console.log(lookUpProfile("Harry", "likes"));
---
+++ codewars/55-lovefunc.js
@@ -0,0 +1,12 @@
+/* 
+==============
+Learn Data Structure with JavaScript
+Coding Challenge 2024 43/366
+Challenge 2024 43. lovefunc
+==============
+*/
+
+const lovefunc = (flower1, flower2) => flower1 % 2 !== flower2 % 2;
+
+console.log(lovefunc(1, 4));
+console.log(lovefunc(2, 2));
